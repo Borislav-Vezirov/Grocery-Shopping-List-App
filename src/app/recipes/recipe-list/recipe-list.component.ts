ba7b5386a82b48e3b72abe5b85224ec198c99f7a
Borislav Vezirov <bobi.vez@gmail.com>
2022-03-22 21:22:19 +0200

    add interface model for recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IRecipe } from 'src/app/shared/interfaces/recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: IRecipe[] = [
+
+    {
+      name: 'This a test',
+      description: 'Test Description',
+      imagePath: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'
+    }
+  ];
 
   constructor() { }
 
---
+++ src/app/shared/interfaces/recipe.ts
@@ -0,0 +1,8 @@
+
+export interface IRecipe {
+
+    name: string;
+    description: string;
+    imagePath: string;
+
+}
